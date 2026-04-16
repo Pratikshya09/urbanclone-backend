153a81b2018e06d8274af553697906104c3de1ae
Pratikshya Nayak <pratikshyanayak962000@gmail.com>
2026-04-17 01:56:38 +0530

    initial commit - spring boot backend

---
+++ src/main/java/com/urbanclone/urbanclonebackend/UrbanclonebackendApplication.java
@@ -0,0 +1,13 @@
+package com.urbanclone.urbanclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanclonebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanclonebackendApplication.class, args);
+	}
+
+}
